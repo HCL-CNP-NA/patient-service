50a770b4e5608e7c079cd1e9915e1db0c5c2c730
Pepe <pboucourt@tmunited.co>
2018-08-01 16:27:49 -0400

    Updating dependencies

--- src/main/java/com/hcl/cnp/patientservice/PatientServiceApplication.java
+++ src/main/java/com/hcl/cnp/patientservice/PatientServiceApplication.java
@@ -2,29 +2,20 @@ package com.hcl.cnp.patientservice;
 
 import com.hcl.cnp.patientservice.domain.Patient;
 import com.hcl.cnp.patientservice.repository.PatientRepository;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import reactor.core.publisher.Flux;
-
-import java.time.Duration;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 @EnableFeignClients
 public class PatientServiceApplication {
 
-	@Bean
-	RouterFunction<?> routes() {
-		return
-				route(GET("/delay"), r -> ok().body(Flux.just("Hello HCL!").delayElements(Duration.ofSeconds(10)), String.class));
-	}
+//	@Bean
+//	RouterFunction<?> routes() {
+//		return
+//				route(GET("/delay"), r -> ok().body(Flux.just("Hello HCL!").delayElements(Duration.ofSeconds(10)), String.class));
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientServiceApplication.class, args);
@@ -36,6 +27,7 @@ public class PatientServiceApplication {
 		repository.add(new Patient(null, "Jose", "Boucourt"));
 		repository.add(new Patient(null, "Ben", "Winston"));
 		repository.add(new Patient(null, "Anil", "Grupta"));
+
 		return repository;
 	}
 }
